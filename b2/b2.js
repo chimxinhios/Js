c2142cbd973d9e217039116ccd5d4d72702a1fcb
chimxinhios <colucnatrat92@gmail.com>
2020-06-18 21:29:57 +0700

    test2

---
+++ b2/b2.js
@@ -0,0 +1,11 @@
+let n = Number('9');
+console.log(`co ${n+n}  con cho `);
+let m = Number(n);
+console.log(m);
+let s = String(198);
+console.log(s + s);
+
+let i = prompt("Nhap so lan lap : ");
+for(let j = 0;j<i;j++){
+    console.log(j);
+}
